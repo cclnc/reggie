6e627b24de64da9288b8499e71938af00cd87c05
cclnc <2238512594@qq.com>
2023-11-10 14:58:28 +0800

    06

--- src/main/java/com/itheima/reggie/common/MyMetaObjecthandler.java
+++ src/main/java/com/itheima/reggie/common/MyMetaObjecthandler.java
@@ -5,17 +5,31 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Component
 @Slf4j
 public class MyMetaObjecthandler implements MetaObjectHandler {
     //插入操作自动填充
     @Override
     public void insertFill(MetaObject metaObject) {
-
+        log.info("公共字段自动填充[insert]...");
+        log.info(metaObject.toString());
+        metaObject.setValue("createTime",LocalDateTime.now());
+        metaObject.setValue("updateTime",LocalDateTime.now());
+        metaObject.setValue("createUser",BaseContext.getCurrentId());
+        metaObject.setValue("createUser",BaseContext.getCurrentId());
     }
     //更新操作自动填充
     @Override
     public void updateFill(MetaObject metaObject) {
+        log.info("公共字段自动填充[update].....");
+        log.info(metaObject.toString());
 
+        Long id = Thread.currentThread().getId();
+        log.info("线程id为：{}",id);
+
+        metaObject.setValue("updateTime", LocalDateTime.now());
+        metaObject.setValue("updataUser",BaseContext.getCurrentId());
     }
 }
