bb67eb184244c394d9b4e90c2d547310d1bd7c85
cclnc <2238512594@qq.com>
2023-12-19 10:52:41 +0800

    6

--- src/main/java/com/itheima/reggie/controller/SetmealController.java
+++ src/main/java/com/itheima/reggie/controller/SetmealController.java
@@ -1,14 +1,20 @@
 package com.itheima.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.dto.SetmealDto;
+import com.itheima.reggie.entity.Category;
+import com.itheima.reggie.entity.Setmeal;
+import com.itheima.reggie.service.CategoryService;
 import com.itheima.reggie.service.SetmealDishService;
 import com.itheima.reggie.service.SetmealService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/setmeal")
@@ -17,13 +23,58 @@ public class SetmealController {
     private SetmealService setmealService;
     @Autowired
     private SetmealDishService setmealDishService;
+    @Autowired
+    private CategoryService categoryService;
     @PostMapping
     public R<String> save(@RequestBody SetmealDto setmealDto){
         setmealService.saveWithDish(setmealDto);
         return R.success("新增套餐成功");
     }
 
+    //套餐分页查询
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize ,String name){
 
+        //分页构造器
+        Page<Setmeal> pageInfo = new Page<>(page,pageSize);
+        Page<SetmealDto> setmealDtoPage =new Page<>();
+        //条件构造器
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        //添加过滤条件
+        queryWrapper.like(name != null,Setmeal::getName,name);
+        //添加排序条件
+        queryWrapper.orderByDesc(Setmeal::getUpdateTime);
+        //进行分页查询
+        setmealService.page(pageInfo,queryWrapper);
 
+        //对象拷贝
+        BeanUtils.copyProperties(pageInfo,setmealDtoPage,"records");
 
+        List<Setmeal> records = pageInfo.getRecords();
+        List<SetmealDto> list = records.stream().map((item)->{
+            SetmealDto setmealDto =new SetmealDto();
+            BeanUtils.copyProperties(item,setmealDto);
+            Long categoryId = item.getCategoryId();//分类Id
+            //根据id查询分类对象
+            Category category = categoryService.getById(categoryId);
+            if (category != null) {
+                //分类名称
+                String categoryName = category.getName();
+                setmealDto.setCategoryName(categoryName);
+            }
+
+            return setmealDto;
+        }).collect(Collectors.toList());
+
+        setmealDtoPage.setRecords(list);
+        return R.success(setmealDtoPage);
+    }
+
+    //删除套餐
+    @DeleteMapping
+    public R<String> delete(@RequestParam List<Long> ids){
+
+        setmealService.removeWithDish(ids);
+        return R.success("套餐数据删除成功");
+    }
 }
